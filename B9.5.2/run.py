3d81a6a92c59e3c39659b3c5444b14ce103cac3d
1050960 <1050960@gmail.com>
2023-04-13 16:42:21 +0500

    B_9.5.2

--- B9.5.2/run.py
+++
@@ -1,6 +0,0 @@
-import timg
-
-obj = timg.Renderer()                                                                                               
-obj.load_image_from_file("test.png")                                                                                
-obj.resize(100,40)
-obj.render(timg.ASCIIMethod)
